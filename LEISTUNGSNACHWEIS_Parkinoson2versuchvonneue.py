f7bbb608a3156c48f32f358fd3d7efa9c972cfc0
SalomeStraumann <131356614+SalomeStraumann@users.noreply.github.com>
2023-05-15 18:38:03 +0200

    Update LEISTUNGSNACHWEIS_Parkinoson2versuchvonneue.py

--- LEISTUNGSNACHWEIS_Parkinoson2versuchvonneue.py
+++ LEISTUNGSNACHWEIS_Parkinoson2versuchvonneue.py
@@ -207,17 +207,22 @@ new_feeling_data = pd.DataFrame(feeling_list)
 new_feeling_data['Datum und Zeit'] = pd.to_datetime(new_feeling_data['Datum und Zeit'])
 new_feeling_data = new_feeling_data.set_index('Datum und Zeit')
 
+
 # Benutzereingabe für die Zeitspanne
 time_periods = ['Letzte Woche', 'Letzter Monat', 'Letzte 3 Monate']
 selected_time_period = st.selectbox('Zeitspanne auswählen:', time_periods)
 
 # Filtere die Daten basierend auf der ausgewählten Zeitspanne
-if selected_time_period == 'Letzte Woche':
+if selected_time_period == 'Heute':
+    filtered_data = new_feeling_data.last('1D')
+elif selected_time_period == 'Letzte Woche':
     filtered_data = new_feeling_data.last('7D')
 elif selected_time_period == 'Letzter Monat':
     filtered_data = new_feeling_data.last('1M')
 elif selected_time_period == 'Letzte 3 Monate':
     filtered_data = new_feeling_data.last('3M')
+elif selected_time_period == 'Letzte 6 Monat':
+    filtered_data = new_feeling_data.last('6M')
 else:
     filtered_data = new_feeling_data  # Kein Filter angewendet
 
@@ -232,7 +237,7 @@ st.line_chart(filtered_data['Stärke der Limitation'])
 
 # Eingabefeld, um regelmÃ¤ssig einzunehmende Medikamente hinzuzufÃ¼gen
 add_current_medication = st.sidebar.text_input(
-    "Medikament hinzufÃ¼gen :blue[regelmÃ¤ssige Einnahme]"
+    "Medikament hinzufügen :blue[regelmÃ¤ssige Einnahme]"
     )
 
 
