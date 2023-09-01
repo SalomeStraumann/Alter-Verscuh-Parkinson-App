16ae6279f52f63dfd870f9d1c51081a745db1e52
parkinson-tracker-app <131356614+SalomeStraumann@users.noreply.github.com>
2023-09-01 21:28:02 +0200

    Update jsonbin.py

--- jsonbin.py
+++ jsonbin.py
@@ -2,105 +2,105 @@ import requests
 
 BIN_API_URL = r'https://api.jsonbin.io/v3/b'
 
-# Funktionen für den "sick"-Teil
+# Funktionen für den "todo"-Teil
 
-def load_data(api_key_sick, bin_id_sick):
+def load_data(api_key_todo, bin_id_todo):
     """
     Lädt den gesamten Bin
     """
-    url_sick = BIN_API_URL + '/' + bin_id_sick + '/latest'
-    headers_sick = {'X-Master-Key': api_key_sick}
-    response_sick = requests.get(url_sick, headers=headers_sick).json()
-    return response_sick['record']
+    url_todo = BIN_API_URL + '/' + bin_id_todo + '/latest'
+    headers_todo = {'X-Master-Key': api_key_todo}
+    response_todo = requests.get(url_todo, headers=headers_todo).json()
+    return response_todo['record']
 
 
-def save_data(api_key_sick, bin_id_sick, data_sick):
+def save_data(api_key_todo, bin_id_todo, data_todo):
     """
     Speichert den gesamten Bin
     """
-    url_sick = BIN_API_URL + '/' + bin_id_sick
-    headers_sick = {'X-Master-Key': api_key_sick, 'Content-Type': 'application/json'}
-    response_sick = requests.put(url_sick, headers=headers_sick, json=data_sick).json()
-    return response_sick
+    url_todo = BIN_API_URL + '/' + bin_id_todo
+    headers_todo = {'X-Master-Key': api_key_todo, 'Content-Type': 'application/json'}
+    response_todo = requests.put(url_todo, headers=headers_todo, json=data_todo).json()
+    return response_todo
 
 
-def load_key(api_key_sick, bin_id_sick, key_sick, empty_value=[]):
+def load_key(api_key_todo, bin_id_todo, key_todo, empty_value=[]):
     """
     Lädt den Wert für einen bestimmten Schlüssel aus dem Bin
     """
-    url_sick = BIN_API_URL + '/' + bin_id_sick + '/latest'
-    headers_sick = {'X-Master-Key': api_key_sick}
-    response_sick = requests.get(url_sick, headers=headers_sick).json()
-    record_sick = response_sick['record']
-    if key_sick in record_sick:
-        return record_sick[key_sick]
+    url_todo = BIN_API_URL + '/' + bin_id_todo + '/latest'
+    headers_todo = {'X-Master-Key': api_key_todo}
+    response_todo = requests.get(url_todo, headers=headers_todo).json()
+    record_todo = response_todo['record']
+    if key_todo in record_todo:
+        return record_todo[key_todo]
     else:
         return empty_value
 
 
-def save_key(api_key_sick, bin_id_sick, key_sick, data_sick):
+def save_key(api_key_todo, bin_id_todo, key_todo, data_todo):
     """
     Speichert den Wert für einen bestimmten Schlüssel im Bin
     """
-    url_sick = BIN_API_URL + '/' + bin_id_sick
-    headers_sick = {'X-Master-Key': api_key_sick, 'Content-Type': 'application/json'}
-    response_sick = requests.get(url_sick, headers=headers_sick).json()
-    record_sick = response_sick['record']
-    if type(record_sick) != dict:
-        record_sick = {key_sick: data_sick}  # erzeuge ein neues Dictionary
+    url_todo = BIN_API_URL + '/' + bin_id_todo
+    headers_todo = {'X-Master-Key': api_key_todo, 'Content-Type': 'application/json'}
+    response_todo = requests.get(url_todo, headers=headers_todo).json()
+    record_todo = response_todo['record']
+    if type(record_todo) != dict:
+        record_todo = {key_todo: data_todo}  # erzeuge ein neues Dictionary
     else:
-        record_sick[key_sick] = data_sick
-    response_sick = requests.put(url_sick, headers=headers_sick, json=record_sick).json()
-    return response_sick
+        record_todo[key_todo] = data_todo
+    response_todo = requests.put(url_todo, headers=headers_todo, json=record_todo).json()
+    return response_todo
 
 
-# Funktionen für den "med"-Teil
+# Funktionen für den "budge"-Teil
 
-def load_data(api_key_med, bin_id_med):
+def load_data(api_key_budge, bin_id_budge):
     """
     Lädt den gesamten Bin
     """
-    url_med = BIN_API_URL + '/' + bin_id_med + '/latest'
-    headers_med = {'X-Master-Key': api_key_med}
-    response_med = requests.get(url_med, headers=headers_med).json()
-    return response_med['record']
+    url_budge = BIN_API_URL + '/' + bin_id_budge + '/latest'
+    headers_budge = {'X-Master-Key': api_key_budge}
+    response_budge = requests.get(url_budge, headers=headers_budge).json()
+    return response_budge['record']
 
 
-def save_data(api_key_med, bin_id_med, data_med):
+def save_data(api_key_budge, bin_id_budge, data_budge):
     """
     Speichert den gesamten Bin
     """
-    url_med = BIN_API_URL + '/' + bin_id_med
-    headers_med = {'X-Master-Key': api_key_med, 'Content-Type': 'application/json'}
-    response_med = requests.put(url_med, headers=headers_med, json=data_med).json()
-    return response_med
+    url_budge = BIN_API_URL + '/' + bin_id_budge
+    headers_budge = {'X-Master-Key': api_key_budge, 'Content-Type': 'application/json'}
+    response_budge = requests.put(url_budge, headers=headers_budge, json=data_budge).json()
+    return response_budge
 
 
-def load_key(api_key_med, bin_id_med, key_med, empty_value=[]):
+def load_key(api_key_budge, bin_id_budge, key_budge, empty_value=[]):
     """
     Lädt den Wert für einen bestimmten Schlüssel aus dem Bin
     """
-    url_med = BIN_API_URL + '/' + bin_id_med + '/latest'
-    headers_med = {'X-Master-Key': api_key_med}
-    response_med = requests.get(url_med, headers=headers_med).json()
-    record_med = response_med['record']
-    if key_med in record_med:
-        return record_med[key_med]
+    url_budge = BIN_API_URL + '/' + bin_id_budge + '/latest'
+    headers_budge = {'X-Master-Key': api_key_budge}
+    response_budge = requests.get(url_budge, headers=headers_budge).json()
+    record_budge = response_budge['record']
+    if key_budge in record_budge:
+        return record_budge[key_budge]
     else:
         return empty_value
 
 
-def save_key(api_key_med, bin_id_med, key_med, data_med):
+def save_key(api_key_budge, bin_id_budge, key_budge, data_budge):
     """
     Speichert den Wert für einen bestimmten Schlüssel im Bin
     """
-    url_med = BIN_API_URL + '/' + bin_id_med
-    headers_med = {'X-Master-Key': api_key_med, 'Content-Type': 'application/json'}
-    response_med = requests.get(url_med, headers=headers_med).json()
-    record_med = response_med['record']
-    if type(record_med) != dict:
-        record_med = {key_med: data_med}  # generate new dict
+    url_budge = BIN_API_URL + '/' + bin_id_budge
+    headers_budge = {'X-Master-Key': api_key_budge, 'Content-Type': 'application/json'}
+    response_budge = requests.get(url_budge, headers=headers_budge).json()
+    record_budge = response_budge['record']
+    if type(record_budge) != dict:
+        record_budge = {key_budge: data_budge}  # generate new dict
     else:
-        record_med[key_med] = data_med
-    response_med = requests.put(url_med, headers=headers_med, json=record_med).json()
-    return response_med
+        record_budge[key_budge] = data_budge
+    response_budge = requests.put(url_budge, headers=headers_budge, json=record_budge).json()
+    return response_budge
